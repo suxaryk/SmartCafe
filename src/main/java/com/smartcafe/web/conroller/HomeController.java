aad26ad285f53d495a2fb8d7c45b1d2124c3116a
suxaryk <suxaryk@ukr.net>
2016-02-13 17:41:36 +0100

    git init base Spring MVC config

---
+++ src/main/java/com/smartcafe/web/conroller/HomeController.java
@@ -0,0 +1,20 @@
+package com.smartcafe.web.conroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+
+    @RequestMapping(value = {"/home", "/"}, method = RequestMethod.GET)
+    public String printWelcome(ModelMap model) {
+
+        model.addAttribute("message", "Spring 4 MVC - Hello World");
+        return "index";
+
+    }
+
+}
